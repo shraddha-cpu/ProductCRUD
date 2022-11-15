41157c235ebb12c080eaffdde696736cd40b82c6
shraddha-cpu <shraddhapaygude5337@gmail.com>
2022-11-15 13:26:36 +0530

    Add project files.

---
+++ ProductCRUD/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ProductCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.txtProductId = new System.Windows.Forms.TextBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtProductPrice = new System.Windows.Forms.TextBox();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.ProductId = new System.Windows.Forms.Label();
+            this.ProductName = new System.Windows.Forms.Label();
+            this.ProductPrice = new System.Windows.Forms.Label();
+            this.CompanyName = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(63, 263);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(208, 263);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(340, 263);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(425, 43);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Location = new System.Drawing.Point(620, 254);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(75, 23);
+            this.btnShowAll.TabIndex = 4;
+            this.btnShowAll.Text = "Show All";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // txtProductId
+            // 
+            this.txtProductId.Location = new System.Drawing.Point(228, 46);
+            this.txtProductId.Name = "txtProductId";
+            this.txtProductId.Size = new System.Drawing.Size(100, 20);
+            this.txtProductId.TabIndex = 5;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(228, 102);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(100, 20);
+            this.txtProductName.TabIndex = 6;
+            // 
+            // txtProductPrice
+            // 
+            this.txtProductPrice.Location = new System.Drawing.Point(228, 163);
+            this.txtProductPrice.Name = "txtProductPrice";
+            this.txtProductPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtProductPrice.TabIndex = 7;
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(228, 212);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(100, 20);
+            this.txtCompanyName.TabIndex = 8;
+            // 
+            // ProductId
+            // 
+            this.ProductId.AutoSize = true;
+            this.ProductId.Location = new System.Drawing.Point(90, 49);
+            this.ProductId.Name = "ProductId";
+            this.ProductId.Size = new System.Drawing.Size(84, 13);
+            this.ProductId.TabIndex = 10;
+            this.ProductId.Text = "Enter Product Id";
+            // 
+            // ProductName
+            // 
+            this.ProductName.AutoSize = true;
+            this.ProductName.Location = new System.Drawing.Point(90, 102);
+            this.ProductName.Name = "ProductName";
+            this.ProductName.Size = new System.Drawing.Size(103, 13);
+            this.ProductName.TabIndex = 11;
+            this.ProductName.Text = "Enter Product Name";
+            // 
+            // ProductPrice
+            // 
+            this.ProductPrice.AutoSize = true;
+            this.ProductPrice.Location = new System.Drawing.Point(90, 170);
+            this.ProductPrice.Name = "ProductPrice";
+            this.ProductPrice.Size = new System.Drawing.Size(99, 13);
+            this.ProductPrice.TabIndex = 12;
+            this.ProductPrice.Text = "Enter Product Price";
+            // 
+            // CompanyName
+            // 
+            this.CompanyName.AutoSize = true;
+            this.CompanyName.Location = new System.Drawing.Point(90, 212);
+            this.CompanyName.Name = "CompanyName";
+            this.CompanyName.Size = new System.Drawing.Size(110, 13);
+            this.CompanyName.TabIndex = 13;
+            this.CompanyName.Text = "Enter Company Name";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(524, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.CompanyName);
+            this.Controls.Add(this.ProductPrice);
+            this.Controls.Add(this.ProductName);
+            this.Controls.Add(this.ProductId);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.txtProductPrice);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.txtProductId);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.TextBox txtProductId;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtProductPrice;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.Label ProductId;
+        private System.Windows.Forms.Label ProductName;
+        private System.Windows.Forms.Label ProductPrice;
+        private System.Windows.Forms.Label CompanyName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ ProductCRUD/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Xml.Linq;
+
+
+namespace ProductCRUD
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlDataAdapter da;
+        SqlCommandBuilder scb;
+        DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string constr = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+            con = new SqlConnection(constr);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public DataSet GetAllEmp()
+        {
+            da = new SqlDataAdapter("select * from Product", con);
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            scb=new SqlCommandBuilder(da);
+            ds = new DataSet();
+            da.Fill(ds, "Product");// Product is a table name given to DataTable
+            return ds;
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllEmp();
+                DataRow row = ds.Tables["Product"].NewRow();
+                row["Pname"] = txtProductName.Text;
+                row["PPrice"] = txtProductPrice.Text;
+                row["CompanyName"] = txtCompanyName.Text;
+                ds.Tables["Product"].Rows.Add(row);
+                int result = da.Update(ds.Tables["Product"]);
+                if (result == 1)
+                {
+                    MessageBox.Show("Record inserted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllEmp();
+                DataRow row = ds.Tables["Product"].Rows.Find(txtProductId.Text);
+                if (row != null)
+                {
+                    row["Pname"] = txtProductName.Text;
+                    row["Pprice"] = txtProductPrice.Text;
+                    row["CompanyName"] = txtCompanyName.Text;
+                    int result = da.Update(ds.Tables["Product"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record updated");
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllEmp();
+                DataRow row = ds.Tables["Product"].Rows.Find(txtProductId.Text);
+                if (row != null)
+                {
+                    row.Delete();
+                    int result = da.Update(ds.Tables["Product"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record deleted");
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllEmp();
+                DataRow row = ds.Tables["Product"].Rows.Find(txtProductId.Text);
+                if (row != null)
+                {
+                    txtProductName.Text = row["Pname"].ToString();
+                    txtProductPrice.Text = row["Pprice"].ToString();
+                    txtCompanyName.Text = row["CompanyName"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Record not found");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllEmp();
+              dataGridView1.DataSource = ds.Tables["Product"];
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
